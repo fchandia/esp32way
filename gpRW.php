c447bf0b0ce85776591eb9621acfe899fc341906
Fernando Chandia Reyes <fchandia@live.com>
2023-01-06 20:21:19 -0300

    Update and rename cmp.php to gpRW.php

---
+++ gpRW.php
@@ -0,0 +1,37 @@
+<?php
+
+if(isset($_GET["g0"])) {
+   $g0 = $_GET["g0"];
+   $g1 = $_GET["g1"];
+   $g2 = $_GET["g2"];
+   $g3 = $_GET["g3"];
+   
+ // get value from HTTP GET
+/* http://lowcostcontrol-production.up.railway.app/gpRW.php?g0=1&g1=1&g2=1&g3=1 */
+
+   $servername = "containers-us-west-142.railway.app";
+   $username = "root";
+   $password = "6EtdzqGS5rdglKSBzCPL";
+   $dbport = "6774";
+   $dbname = "esp32";
+
+   // Create connection
+   $conn = new mysqli($servername.':'.$dbport, $username, $password, $dbname);
+   // Check connection
+   if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+   }
+
+   $sql = "UPDATE gpios SET gpio0 = '" . $g0 . "', gpio1 = '" . $g1 . "', gpio2='" . $g2 . "', gpio3='" . $g3 . "')";
+
+   if ($conn->query($sql) === TRUE) {
+      echo 0;
+   } else {
+      echo "Error: " . $sql . " => " . $conn->error;
+   }
+
+   $conn->close();
+} else {
+   echo 1;
+}
+?>
